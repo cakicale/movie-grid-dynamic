e2521c8aba500873c9bffec7c09d6ad16e947226
cakicale <caki01994@gmail.com>
2020-12-24 19:19:59 +0100

    dynamic elements

--- javascript.js
+++ javascript.js
@@ -46,8 +46,84 @@ const fetchMoviesJSON = async () => {
 //function used with fetch
 
 const showData = async () => {
+
+    let container = document.querySelector("#container");
     const data = await fetchMoviesJSON();
     for (let prop in data) {
+        //html elements
+        let divItem = document.createElement("div");
+        divItem.setAttribute("class", "item1");
+        container.append(divItem);
+
+        //form
+        let threeDotButton = document.createElement("button");
+        threeDotButton.setAttribute("id", "btn" + prop);
+        threeDotButton.setAttribute("class", "iconify");
+        threeDotButton.setAttribute("data-icon", "entypo-dots-three-horizontal");
+        threeDotButton.setAttribute("data-inline", "false");
+        divItem.append(threeDotButton);
+
+        let divForm = document.createElement("div");
+        divForm.setAttribute("class", "form-popup");
+        divForm.setAttribute("id", "myForm" + prop);
+        divItem.append(divForm);
+
+        let pFormText1 = document.createElement("p");
+        pFormText1.innerHTML = "Want to rate or add this item to a list?";
+        pFormText1.style.fontWeight = 900;
+        divForm.append(pFormText1);
+
+        let a1 = document.createElement("a");
+        a1.innerHTML = "Login";
+        a1.setAttribute("href", "#");
+        divForm.append(a1);
+
+        let pFormText2 = document.createElement("p");
+        pFormText2.innerHTML = "Not a member?";
+        pFormText2.style.fontWeight = 900;
+        divForm.append(pFormText2);
+
+        let a2 = document.createElement("a");
+        a2.innerHTML = "Sign up and join the community";
+        a2.setAttribute("href", "#");
+        divForm.append(a2);
+
+        //movie
+        let imgMovie = document.createElement("img");
+        imgMovie.setAttribute("src", "slike/" + prop + ".jpg");
+        divItem.append(imgMovie);
+        //movie text
+        let divMovieText = document.createElement("div");
+        divMovieText.setAttribute("class", "text");
+        divItem.append(divMovieText);
+
+        let span1 = document.createElement("span");
+        span1.setAttribute("class", "dot");
+        divMovieText.append(span1);
+        let span2 = document.createElement("span");
+        span2.setAttribute("class", "inner-circle");
+        divMovieText.append(span2);
+        let spanPercentNumber = document.createElement("span");
+        spanPercentNumber.setAttribute("class", "procenat");
+        span2.append(spanPercentNumber);
+        let spanPercentSign = document.createElement("span");
+        spanPercentSign.setAttribute("class", "percent");
+        span2.append(spanPercentSign);
+
+        let movieTitle = document.createElement("h2");
+        divMovieText.append(movieTitle);
+        let aTitle = document.createElement("a");
+        aTitle.setAttribute("href", "#");
+        movieTitle.append(aTitle);
+        let pDate = document.createElement("p");
+        pDate.setAttribute("class", "date");
+        pDate.innerHTML = "Jul 31, 2020";
+        divMovieText.append(pDate);
+        let pOverview = document.createElement("p");
+        pOverview.setAttribute("class", "overview");
+        divMovieText.append(pOverview);
+
+        //data
         selectTitle[prop].innerHTML = data[prop].title;
         selectDesc[prop].innerHTML = data[prop].description;
         selectRating[prop].innerHTML = data[prop].rating;
@@ -84,81 +160,3 @@ selectInput.addEventListener('input', showSearchedMovies);
 //calling functions
 showData();
 
-//////////////////////////////////////////
-//dynamic html
-let container = document.querySelector("#container");
-for (let i = 1; i < 13; i++) {
-
-
-    let divItem = document.createElement("div");
-    divItem.setAttribute("class", "item1");
-    container.append(divItem);
-
-    //form
-    let threeDotButton = document.createElement("button");
-    threeDotButton.setAttribute("id", "btn" + i);
-    threeDotButton.setAttribute("class", "iconify");
-    threeDotButton.setAttribute("data-icon", "entypo-dots-three-horizontal");
-    threeDotButton.setAttribute("data-inline", "false");
-    divItem.append(threeDotButton);
-
-    let divForm = document.createElement("div");
-    divForm.setAttribute("class", "form-popup");
-    divForm.setAttribute("id", "myForm" + i);
-    divItem.append(divForm);
-
-    let pFormText1 = document.createElement("p");
-    pFormText1.innerHTML = "Want to rate or add this item to a list?";
-    pFormText1.style.fontWeight = 900;
-    divForm.append(pFormText1);
-
-    let a1 = document.createElement("a");
-    a1.innerHTML = "Login";
-    a1.setAttribute("href", "#");
-    divForm.append(a1);
-
-    let pFormText2 = document.createElement("p");
-    pFormText2.innerHTML = "Not a member?";
-    pFormText2.style.fontWeight = 900;
-    divForm.append(pFormText2);
-
-    let a2 = document.createElement("a");
-    a2.innerHTML = "Sign up and join the community";
-    a2.setAttribute("href", "#");
-    divForm.append(a2);
-
-    //movie
-    let imgMovie = document.createElement("img");
-    imgMovie.setAttribute("src", "slike/" + i + ".jpg");
-    divItem.append(imgMovie);
-    //movie text
-    let divMovieText = document.createElement("div");
-    divMovieText.setAttribute("class", "text");
-    divItem.append(divMovieText);
-
-    let span1 = document.createElement("span");
-    span1.setAttribute("class", "dot");
-    divMovieText.append(span1);
-    let span2 = document.createElement("span");
-    span2.setAttribute("class", "inner-circle");
-    divMovieText.append(span2);
-    let spanPercentNumber = document.createElement("span");
-    spanPercentNumber.setAttribute("class", "procenat");
-    span2.append(spanPercentNumber);
-    let spanPercentSign = document.createElement("span");
-    spanPercentSign.setAttribute("class", "percent");
-    span2.append(spanPercentSign);
-
-    let movieTitle = document.createElement("h2");
-    divMovieText.append(movieTitle);
-    let aTitle = document.createElement("a");
-    aTitle.setAttribute("href", "#");
-    movieTitle.append(aTitle);
-    let pDate = document.createElement("p");
-    pDate.setAttribute("class", "date");
-    pDate.innerHTML = "Jul 31, 2020";
-    divMovieText.append(pDate);
-    let pOverview = document.createElement("p");
-    pOverview.setAttribute("class", "overview");
-    divMovieText.append(pOverview);
-}
